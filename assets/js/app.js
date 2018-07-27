8b418447d464848b9bb574d02277214b64bf73b7
Hunter Stutts <stuttskyle@gmail.com>
2018-07-26 21:16:57 -0500

    Got rid of URL links

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 var topGames = $(this).attr("top-games");
 
 // Constructing a queryURL using the top games
-var queryURL = "https://api-endpoint.igdb.com/games/?fields=name,url,popularity&order=popularity:desc&limit=5"
+var queryURL = "https://api-endpoint.igdb.com/games/?fields=name,total_rating&order=total_rating:desc&limit=5"
 
 var proxy = 'https://cors-anywhere.herokuapp.com/';
 
@@ -18,40 +18,21 @@ $.ajax({
     .then(function (response) {
         console.log(response);
 
-        //This is an alternate to the for loop below
-        //arrow function with for each array method.
+        //Lines 21-31 are an alternate to the for map function below
+        //Lines 22-26 are an arrow function with for each array method.
         // response.forEach(game => {
         //     console.log(game)
         //     $("#topGames").append(game.name);
         // });
-
-        response.map(function(val){
-            return $('#topGames').append(`<li><a href=${val.url}>${val.name}</a></li>`);
-        })
 
         // for (var i = 0; i < response.length; i++) {
         //     console.log(response[i].name)
         //     $('#orderedGames').append(`<li><a href=${response[i].url}>${response[i].name}</a></li>`);
         // };
 
-        // storing the data from the AJAX request in the results variable
-        // var results = response.data;
-        // Looping through each result item
-        // for (var i = 0; i < results.length; i++) {
-
-        // Creating and storing a div tag
-        // var gamesDiv = $("<div>");
-        // console.log(results[i]);
-        // Creating a paragraph tag with the result item's rating
-        // var p = $("<p>").text("Top 5 Games: " + results[i].rating);
+        response.map(function(val){
+            return $('#topGames').append(`<li>${val.name}</li>`);
+        })
 
 
-        // Creating and storing an image tag
-        // var gamesImage = $("<img>");
-        // Setting the src attribute of the image to a property pulled off the result item
-        // gamesImage.attr("src", results[i].images.fixed_height.url);
-
-        // Appending the paragraph and image tag to the showsDiv
-        // gamesDiv.append(p);
-        // gamesDiv.append(gamesImage);
     });
