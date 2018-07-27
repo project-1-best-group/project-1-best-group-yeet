d5e71a93eb2737b7e13ad6f28f7548ec33542aec
Hunter Stutts <stuttskyle@gmail.com>
2018-07-26 19:00:36 -0500

    Words have magically appeared, send halp

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,35 +16,41 @@ $.ajax({
     })
     // After data comes back from the request
     .then(function (response) {
-        console.log(queryURL);
-
+        // console.log(queryURL);
         console.log(response);
-
+        // console.log(response);
+        // var json = JSON.parse(response);
+        // console.log(response[0]);
         // Transfer content to HTML
-        $("#topGames").text("Top 5 Games: " + response);
 
-        var json = JSON.parse('#topGames');
-        console.log(json.name);
+        //arrow function with for each array method.
+        // response.forEach(game => {
+        //     console.log(game)
+        //     $("#topGames").append(game.name);
+        // });
+        for (var i = 0; i < response.length; i++) {
+            console.log(response[i].name)
+            $("#topGames").append(response[i].name);
+        }
 
         // storing the data from the AJAX request in the results variable
         // var results = response.data;
         // Looping through each result item
         // for (var i = 0; i < results.length; i++) {
 
-            // Creating and storing a div tag
-            // var gamesDiv = $("<div>");
-            // console.log(results[i]);
-            // Creating a paragraph tag with the result item's rating
-            // var p = $("<p>").text("Top 5 Games: " + results[i].rating);
+        // Creating and storing a div tag
+        // var gamesDiv = $("<div>");
+        // console.log(results[i]);
+        // Creating a paragraph tag with the result item's rating
+        // var p = $("<p>").text("Top 5 Games: " + results[i].rating);
 
 
-            // Creating and storing an image tag
-            // var gamesImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            // gamesImage.attr("src", results[i].images.fixed_height.url);
+        // Creating and storing an image tag
+        // var gamesImage = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+        // gamesImage.attr("src", results[i].images.fixed_height.url);
 
-            // Appending the paragraph and image tag to the showsDiv
-            // gamesDiv.append(p);
-            // gamesDiv.append(gamesImage);
-        }
-    );
+        // Appending the paragraph and image tag to the showsDiv
+        // gamesDiv.append(p);
+        // gamesDiv.append(gamesImage);
+    });
